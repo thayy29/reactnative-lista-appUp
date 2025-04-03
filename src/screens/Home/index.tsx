ab70a92df6b1de6203f2def5588b367e0b7abdf5
Thayana <thayana.cs@gmail.com>
2025-04-03 17:40:44 -0300

    add input and button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export default function Home() {
+   
+  const handleOnPress = () => {
+    console.log("Adicionou!")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>This is my App</Text>
-      <Text style={styles.secondaryText}>hello world!</Text>
+      <Text style={styles.secondaryText}>Adicione um participante!</Text>
+      <View style={styles.box}>
+        <TextInput
+        placeholder="Escreva o nome de um participante"
+        style={styles.textInput}
+        />
+
+        <TouchableOpacity style={styles.btnText} onPress={handleOnPress}>
+            <Text>+</Text>
+          </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -12,11 +12,40 @@ export const styles = StyleSheet.create({
     color: "white",
     paddingTop: 10,
     fontSize: 20,
+    alignItems: "flex-start"
   },
 
   secondaryText: {
     color: "gray",
     paddingTop: 1,
-    fontSize: 20,
+    fontSize: 16,
+    justifyContent: "flex-start"
   },
+
+  textInput : {
+    backgroundColor: "#2f2f33",
+    marginTop: 12,
+    color: "#ffffff",
+    padding: 16,
+    fontSize: 16,
+    borderRadius: 10,
+    outlineColor: "#2f2f33",
+  },
+
+  btnText: {
+    backgroundColor: "#76e070",
+    width: 50,
+    height: 50,
+    padding: 20,
+    marginTop: 5,
+    borderRadius: 10,
+    fontSize:  16,
+    justifyContent: "center"
+  },
+  box: {
+    flexDirection: "row-reverse",
+    justifyContent: "space-around",
+    alignItems: "center",
+  }
+
 });
